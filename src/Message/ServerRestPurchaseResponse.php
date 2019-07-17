b1cb956f7e4540258a2e6ea0924a2e928839d411
Ben James <in@benjam.es>
2019-07-17 16:18:02 +0100

    building data
    Signed-off-by: Ben James <in@benjam.es>

---
+++ src/Message/ServerRestPurchaseResponse.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Omnipay\SagePay\Message;
+
+/**
+ * Sage Pay REST Server Purchase Response
+ */
+class ServerRestPurchaseResponse extends Response
+{
+    /**
+     * @return bool false
+     */
+    public function isSuccessful()
+    {
+        //
+    }
+}
